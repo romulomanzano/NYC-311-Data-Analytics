e436801aae821a076cc6d0f678652afb7484e950
romulomanzano <romulomanzano@gmail.com>
2017-07-29 15:18:29 -0400

    Initial commit of baseline code
    Baseline experiment downloading data via SODA API in Python

---
+++ src/run_data_socrata_api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 29 14:34:41 2017
+
+@author: romulo
+"""
+
+from sodapy import Socrata
+from socrata_311 import settings
+
+#Setting up the initial connector
+client = Socrata(settings.APP_NYC_API_DOMAIN ,settings.APP_TOKEN_311,timeout=90)
+
+#Getting a sense of the metadata available for this dataset
+meta = client.get_metadata(settings.APP_NYC_DATASET)
+
+#some experiments about downloading data based on key
+data_key = client.get(settings.APP_NYC_DATASET,limit = 100,where = "unique_key == '36805062'")
+#some experiments about downloading data based on date
+dataDate = client.get(settings.APP_NYC_DATASET,limit = 1000,where = "created_date >= '2017-07-28'")
+#some experiments about downloading data based on timestamp
+dataTimestamp = client.get(settings.APP_NYC_DATASET,limit = 10000,where = "created_date >= '2017-07-27T00:01:02.000'")
+
---
+++ src/socrata_311/settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 29 14:40:28 2017
+
+@author: romulo
+"""
+
+APP_TOKEN_311 = 'VQtPna4DyINdAHU8AgxFGwiEr'
+APP_NYC_DATASET = 'fhrw-4uyv'
+APP_NYC_API_DOMAIN = 'data.cityofnewyork.us'
